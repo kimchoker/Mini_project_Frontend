5130b089e22a11ded97ab6e4d38c308a09cceccf
Junhyuk Kim <121940381+kimchoker@users.noreply.github.com>
2023-05-16 21:08:15 +0900

    태훈씨거 넣음

--- src/App.js
+++ src/App.js
@@ -47,6 +47,14 @@ export default function App() {
           <Standings />
           <Footer />
         </>} />
+        <Route path="/homeplate/write" element={<>
+          <Navigation />
+          <Write/>
+        </>}/>
+        <Route path="/homeplate/search/:word" element={<>
+          <Navigation />
+          <Search/>
+        </>}/>
         <Route path="/schedule" element={<>
           <Navigation />
           <Schedule />
